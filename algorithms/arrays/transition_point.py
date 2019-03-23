a64102cc2c06b62b07921116c31fdb350354ba1a
bhaveshAn <bhaveshanand7@gmail.com>
2019-03-23 12:13:13 +0530

    Modify transition point prblm

--- algorithms/arrays/transition_point.py
+++ algorithms/arrays/transition_point.py
@@ -3,13 +3,14 @@ def transition_point(arr, n):
     r = n - 1
     while l <= r:
         mid = (l + r) // 2
-        if arr[mid] == 1 and arr[mid - 1] == 0:
-            return mid
-        elif arr[mid] < 1:
+        if arr[mid] == 0:
             l = mid + 1
+        elif arr[mid] == 1:
+            if arr[mid-1] == 0:
+                return mid
         else:
             r = mid - 1
-    return
+    return -1
 
 
 arr = [0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1]
